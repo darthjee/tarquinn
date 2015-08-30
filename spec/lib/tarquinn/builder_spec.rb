2c101af4a98234722947ae4316320d631c1df201
darthjee favini <darthjee@gmail.com>
2015-08-30 19:00:42 -0300

    Add spec over builder#build

---
+++ spec/lib/tarquinn/builder_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Tarquinn::Builder do
+  describe '#build' do
+    let(:controller) do
+      double('controller')
+    end
+
+    it do
+      expect(subject.build(controller)).to be_a(Tarquinn::Engine)
+    end
+  end
+end
