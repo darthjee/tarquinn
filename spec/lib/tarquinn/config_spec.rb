c06f838d514629c8b17552b590e3ad945b562f8a
darthjee favini <darthjee@gmail.com>
2015-08-30 23:36:31 -0300

    Generate blocks from methods names

--- spec/lib/tarquinn/config_spec.rb
+++ spec/lib/tarquinn/config_spec.rb
@@ -8,27 +8,57 @@ describe Tarquinn::Config do
       it do
         expect do
           subject.add_redirection_rules(:methods)
-        end.to change { subject.methods }
+        end.to change { subject.redirection_blocks }
       end
 
       it do
         expect do
           subject.add_redirection_rules(:methods)
-        end.not_to change { subject.blocks }
+        end.not_to change { subject.skip_blocks }
       end
     end
 
-    context 'when not passing only a block' do
+    context 'when passing only a block' do
       it do
         expect do
           subject.add_redirection_rules { true }
-        end.not_to change { subject.methods }
+        end.to change { subject.redirection_blocks }
       end
 
       it do
         expect do
           subject.add_redirection_rules { true }
-        end.to change { subject.blocks }
+        end.not_to change { subject.skip_blocks }
+      end
+    end
+  end
+
+  describe '#add_skip_rules' do
+    context 'when not passing a block' do
+      it do
+        expect do
+          subject.add_skip_rules(:methods)
+        end.to change { subject.skip_blocks }
+      end
+
+      it do
+        expect do
+          subject.add_skip_rules(:methods)
+        end.not_to change { subject.redirection_blocks }
+      end
+    end
+
+    context 'when passing only a block' do
+      it do
+        expect do
+          subject.add_skip_rules { true }
+        end.to change { subject.skip_blocks }
+      end
+
+      it do
+        expect do
+          subject.add_skip_rules { true }
+        end.not_to change { subject.redirection_blocks }
       end
     end
   end
