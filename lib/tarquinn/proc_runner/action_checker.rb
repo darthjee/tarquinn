418662d2e8da6b5652f099aaf4f6f611029c79a1
darthjee favini <darthjee@gmail.com>
2015-09-02 00:40:45 -0300

    Organize action checker

--- lib/tarquinn/proc_runner/action_checker.rb
+++ lib/tarquinn/proc_runner/action_checker.rb
@@ -1,8 +1,17 @@
 class Tarquinn::ProcRunner::ActionChecker < Tarquinn::ProcRunner
+  attr_accessor :routes
+
   def initialize(routes)
-    routes = [ routes ].flatten.map(&:to_s)
-    super() do |controller|
-      routes.include? controller.params[:action]
+    @routes = [ routes ].flatten.map(&:to_s)
+
+    super(&build_block)
+  end
+
+  private
+
+  def build_block
+    Proc.new do |controller|
+      self.routes.include? controller.params[:action]
     end
   end
 end
