16deda7504ae2493347b5c3c58654cc8db1a5359
darthjee favini <darthjee@gmail.com>
2015-09-03 23:50:00 -0300

    Extract common code for example

--- spec/support/shared_examples/config.rb
+++ spec/support/shared_examples/config.rb
@@ -1,37 +1,28 @@
 shared_examples 'a method that adds a redirection rule' do |expected_class|
-  it do
-    expect do
-      call_method
-    end.to change { subject.redirection_blocks }
-  end
-
-  it do
-    call_method
-    expect(subject.redirection_blocks.last).to be_a(expected_class)
-  end
-
-  it do
-    expect do
-      call_method
-    end.not_to change { subject.skip_blocks }
-  end
+  it_behaves_like 'a method that adds a rule', :redirection, expected_class
 end
 
 shared_examples 'a method that adds a skip rule' do |expected_class|
+  it_behaves_like 'a method that adds a rule', :skip, expected_class
+end
+
+shared_examples 'a method that adds a rule' do |rule, expected_class|
+  let(:reverse_rule) { rule == :skip ? :redirection : :skip }
   it do
     expect do
       call_method
-    end.to change { subject.skip_blocks }
+    end.to change { subject.public_send("#{rule}_blocks" ) }
   end
 
   it do
     call_method
-    expect(subject.skip_blocks.last).to be_a(expected_class)
+    expect(subject.public_send("#{rule}_blocks" ).last).to be_a(expected_class)
   end
 
   it do
     expect do
       call_method
-    end.not_to change { subject.redirection_blocks }
+    end.not_to change { subject.public_send("#{reverse_rule}_blocks" ) }
   end
 end
+
