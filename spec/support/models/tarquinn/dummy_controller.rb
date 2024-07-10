06de961069816b3ae739b1a243d3db3504f852d0
darthjee <darthjee@gmail.com>
2024-07-10 13:50:06 -0300

    Enable Style/ClassAndModuleChildren

--- spec/support/models/tarquinn/dummy_controller.rb
+++ spec/support/models/tarquinn/dummy_controller.rb
@@ -1,45 +1,47 @@
 # frozen_string_literal: true
 
-class Tarquinn::DummyController
-  def self.before_action(_)
-  end
+module Tarquinn
+  class DummyController
+    def self.before_action(_)
+    end
 
-  include Tarquinn
+    include Tarquinn
 
-  skip_redirection :redirection_path, :route_method
-  redirection_rule :redirection_path, :should_redirect?
-  skip_redirection_rule :redirection_path, :should_skip_redirect?
+    skip_redirection :redirection_path, :route_method
+    redirection_rule :redirection_path, :should_redirect?
+    skip_redirection_rule :redirection_path, :should_skip_redirect?
 
-  def parse_request
-    perform_redirection
-  end
+    def parse_request
+      perform_redirection
+    end
 
-  private
+    private
 
-  def params
-    { action: 'show' }
-  end
+    def params
+      { action: 'show' }
+    end
 
-  def true
-    true
-  end
+    def true
+      true
+    end
 
-  def false
-    false
-  end
+    def false
+      false
+    end
 
-  def redirection_path
-    '/path'
-  end
+    def redirection_path
+      '/path'
+    end
 
-  def redirect_to(_)
-  end
+    def redirect_to(_)
+    end
 
-  def should_redirect?
-    true
-  end
+    def should_redirect?
+      true
+    end
 
-  def should_skip_redirect?
-    false
+    def should_skip_redirect?
+      false
+    end
   end
 end
