ef0001dcd51fb1474b6344252e8295c12f11cb0d
darthjee <darthjee@gmail.com>
2024-07-12 18:05:05 -0300

    Add method documentation

--- lib/tarquinn.rb
+++ lib/tarquinn.rb
@@ -18,12 +18,51 @@ module Tarquinn
   require 'tarquinn/builder'
   require 'tarquinn/class_methods'
 
+  # @method self.redirection_rule(redirection, *methods, &block)
+  #
+  # Creates a redirection rule
+  #
+  # The rule name defines which method will be called when checking the path of redirection
+  #
+  # @param (see Tarquinn::ClassMethods#redirection_rule)
+  # @return (see Tarquinn::ClassMethods#redirection_rule)
+
+  # @method self.skip_redirection(redirection, *actions)
+  #
+  # Attaches a condition to skip a redirection based on route (controller action)
+  #
+  # When any of the skip rules is met the redirection is skipped
+  #
+  # @param (see Tarquinn::ClassMethods#skip_redirection)
+  # @return (see Tarquinn::ClassMethods#skip_redirection)
+
+  # @method self.skip_redirection_rule(redirection, *methods, &block)
+  #
+  # Attaches conditions to skip a redirection
+  #
+  # Methods and blocks are ran and if any returns true, the redirec is skipped
+  #
+  # @param (see Tarquinn::ClassMethods#skip_redirection)
+  # @return (see Tarquinn::ClassMethods#skip_redirection)
+
+  # @method self.redirector_builder
+  #
+  # Retruns the Engine Builder
+  #
+  # Engine Builder will Carry all the configurations and will create
+  # one {Engine} for each request
+  #
+  # @return (see Tarquinn::ClassMethods#redirector_builder)
   included do
     before_action :perform_redirection
   end
 
   private
 
+  # @api private
+  # private
+  #
+  # @return [Tarquinn::Engine] an engine for the controller
   def redirector_engine
     self.class.redirector_builder.build(self)
   end
--- lib/tarquinn/class_methods.rb
+++ lib/tarquinn/class_methods.rb
@@ -5,18 +5,48 @@ module Tarquinn
   #
   # Methods added by Tarquinn
   module ClassMethods
-    def skip_redirection(redirection, *actions)
-      redirector_builder.add_skip_action(redirection, *actions)
-    end
-
+    # Creates a redirection rule
+    #
+    # The rule name defines which method will be called when checking the path of redirection
+    #
+    # @param redirection [Symbol] Rule name and method with redirection path
+    # @param methods [Array<Symbol>] Methods that tell that a redirection should be applied
+    # @param block [Proc] block that tells if a the redirection should be applied
+    #
+    # @return [NilClass,Array] Current registered conditions
     def redirection_rule(redirection, *methods, &block)
       redirector_builder.add_redirection_config(redirection, *methods, block)
     end
 
+    # Attaches a condition to skip a redirection based on route (controller action)
+    #
+    # When any of the skip rules is met the redirection is skipped
+    #
+    # @param redirection [Symbol] Rule name to attach the skip condition
+    # @param actions [Array<Symbol>] Route actions to be skipped
+    #
+    # @return [NilClass,Array] Current registered conditions
+    def skip_redirection(redirection, *actions)
+      redirector_builder.add_skip_action(redirection, *actions)
+    end
+
+    # Attaches conditions to skip a redirection
+    #
+    # Methods and blocks are ran and if any returns true, the redirec is skipped
+    #
+    # @param redirection [Symbol] Rule name to attach the skip condition
+    # @param methods [Array<Symbol>] Methods that tell that a redirection should be skipped
+    # @param block [Proc] block that tells if a the redirection should be skipped
     def skip_redirection_rule(redirection, *methods, &block)
       redirector_builder.add_skip_config(redirection, *methods, block)
     end
 
+    # Retruns the Engine Builder
+    #
+    # Engine Builder will Carry all the configurations and will create
+    # one {Engine} for each request
+    #
+    # @return [Builder]
     def redirector_builder
       @redirector_builder ||= Tarquinn::Builder.new
     end
