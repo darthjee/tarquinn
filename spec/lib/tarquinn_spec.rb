639bfbbef05d30aea6bb399305c7cb1b0babdd12
darthjee <darthjee@gmail.com>
2024-07-13 21:35:12 -0300

    Refactor spec

--- spec/lib/tarquinn_spec.rb
+++ spec/lib/tarquinn_spec.rb
@@ -4,56 +4,54 @@ require 'spec_helper'
 
 describe Tarquinn, type: :controller do
   describe '.redirection_rule' do
-    context 'with basic rule' do
-      controller(Tarquinn::DummyRouteController) do
-        redirection_rule :redirection, :should_redirect?
-      end
+    controller(Tarquinn::DummyRouteController) do
+      redirection_rule :redirection, :should_redirect?
+    end
 
-      before do
-        get :index, params: parameters
-      end
+    before do
+      get :index, params: parameters
+    end
 
-      context 'when request indicates a redirection' do
-        let(:parameters) { { should_redirect: true } }
+    context 'when request indicates a redirection' do
+      let(:parameters) { { should_redirect: true } }
 
-        it 'performs a redirect' do
-          expect(response).to redirect_to('/path')
-        end
-      end
-
-      context 'when request does not indicate a redirection' do
-        let(:parameters) { {} }
-
-        it 'does not performs a redirect' do
-          expect(response).not_to redirect_to('/path')
-        end
+      it 'performs a redirect' do
+        expect(response).to redirect_to('/path')
       end
     end
 
-    context 'with skip action' do
-      controller(Tarquinn::DummyRouteController) do
-        redirection_rule :redirection, :always_redirect
-        skip_redirection :redirection, :index
+    context 'when request does not indicate a redirection' do
+      let(:parameters) { {} }
+
+      it 'does not performs a redirect' do
+        expect(response).not_to redirect_to('/path')
       end
+    end
+  end
 
-      before do
-        get action
+  describe '.skip_redirection_rule' do
+    controller(Tarquinn::DummyRouteController) do
+      redirection_rule :redirection, :always_redirect
+      skip_redirection :redirection, :index, :delete
+    end
+
+    before do
+      get action
+    end
+
+    context 'when requestin a path that redirects' do
+      let(:action) { :new }
+
+      it 'performs a redirect' do
+        expect(response).to redirect_to('/path')
       end
+    end
 
-      context 'when requestin a path that redirects' do
-        let(:action) { :new }
+    context 'when request does not indicate a redirection' do
+      let(:action) { :index }
 
-        it 'performs a redirect' do
-          expect(response).to redirect_to('/path')
-        end
-      end
-
-      context 'when request does not indicate a redirection' do
-        let(:action) { :index }
-
-        it 'does not performs a redirect' do
-          expect(response).not_to redirect_to('/path')
-        end
+      it 'does not performs a redirect' do
+        expect(response).not_to redirect_to('/path')
       end
     end
   end
