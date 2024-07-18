42a3e803528a90629ce23de0978624ba57525cdb
darthjee <darthjee@gmail.com>
2024-07-18 15:53:16 -0300

    Add documentation

--- lib/tarquinn/controller.rb
+++ lib/tarquinn/controller.rb
@@ -28,10 +28,21 @@ module Tarquinn
       controller.send(method, *args, **opts, &block)
     end
 
+    # Run a block in teh context of the controller
+    #
+    # @param block [Proc] block to be ran
+    #
+    # @return [Object] the return of the block
     def run(&block)
       controller.send(:instance_eval, &block)
     end
 
+    # Checks if the controller responds to a method
+    #
+    # @param method [Symbol] method name
+    #
+    # @return [TrueClass] the controller responds to this method
+    # @return [FalseClass] the controller does not responds to this method
     def method?(method)
       controller.respond_to?(method, true)
     end
@@ -39,5 +50,13 @@ module Tarquinn
     private
 
     attr_reader :controller
+
+    # @method controller
+    # @api private
+    # @private
+    #
+    # Returns the controller handling the request
+    #
+    # @return [ActionController::Base]
   end
 end
