59a2c936850cf7d607c073dda34e977d8cf3e200
Fernando Favini <darthjee@gmail.com>
2026-02-25 10:58:03 -0300

    Add doc

--- lib/tarquinn/exception.rb
+++ lib/tarquinn/exception.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 module Tarquinn
+  # @api public
+  #
+  # Exceptions raised by +Tarquinn+
   module Exception
+    # @api public
+    #
+    # Raised when trying to create a redirection with a name already defined
     class RedirectionAlreadyDefined < StandardError
+      # @api private
+      #
+      # @param redirection [Symbol] Redirection name that is already defined
       def initialize(redirection)
         super("Redirection #{redirection} already defined")
       end
