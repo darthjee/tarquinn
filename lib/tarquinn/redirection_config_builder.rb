1f2ea2bc6ccc3e6177be090ed7d16cebee5b4149
Fernando Favini <darthjee@gmail.com>
2026-02-25 14:39:51 -0300

    Add build

--- lib/tarquinn/redirection_config_builder.rb
+++ lib/tarquinn/redirection_config_builder.rb
@@ -3,5 +3,24 @@
 module Tarquinn
   class RedirectionConfigBuilder < Sinclair::Model
     initialize_with :configs, :redirection, :methods, :block
+
+    def self.build(...)
+    end
+
+    def build
+      create_config do |config|
+        config.add_redirection_rules(*methods, &block)
+      end
+    end
+
+    private
+
+    def create_config
+      raise Exception::RedirectionAlreadyDefined, redirection if configs[redirection.to_sym]
+
+      config = configs[redirection.to_sym] = Tarquinn::RedirectionConfig.new(redirection)
+
+      config.tap(&block)
+    end
   end
 end
