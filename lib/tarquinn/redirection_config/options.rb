bbf2c5429793784391c41b4452dc7695d8af4a04
darthjee <darthjee@gmail.com>
2026-02-26 20:34:00 -0300

    Init doc

--- lib/tarquinn/redirection_config/options.rb
+++ lib/tarquinn/redirection_config/options.rb
@@ -2,6 +2,11 @@
 
 module Tarquinn
   class RedirectionConfig
+    # Options for the RedirectionConfig class.
+    #
+    # These will be implemented in the future
+    #
+    # @see Tarquinn::RedirectionConfig
     class Options < Sinclair::Options
     end
   end
