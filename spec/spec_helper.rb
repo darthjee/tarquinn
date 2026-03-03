e2d2d2b22839573d753842db89b3ff9f4953d861
Fernando Favini <darthjee@gmail.com>
2026-03-03 18:17:17 -0300

    Report Coverage (#35)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,12 @@
 
 require 'simplecov'
 
+if ENV['CI']
+  require 'simplecov-lcov'
+  SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+  SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+end
+
 SimpleCov.start do
   add_filter '/spec/'
 end
